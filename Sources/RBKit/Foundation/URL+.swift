8a27f58a15677bdfec6e905393a885f5e673bf16
roeybiran <roeybiran@icloud.com>
2023-10-31 15:34:37 +0200

    fixes

--- Sources/RBKit/Foundation/URL+.swift
+++ Sources/RBKit/Foundation/URL+.swift
@@ -4,16 +4,17 @@ import Foundation
 // https://blog.eidinger.info/use-new-url-related-apis-from-ios-16-in-lower-versions
 // https://alejandromp.com/blog/backport-swiftui-modifiers
 
-@available(macOS, obsoleted: 13.0, message: "")
 extension URL {
-  public init?(filePath: String, isDirectory: Bool, relativeTo base: URL? = nil) {
+  @available(macOS, introduced: 10.10, obsoleted: 13.0, message: "")
+  public init(filePath path: String, directoryHint: URL._DirectoryHint = .inferFromPath, relativeTo base: URL? = nil) {
     if #available(macOS 13.0, *) {
-      self = URL(filePath: filePath, directoryHint: isDirectory ? .isDirectory : .notDirectory, relativeTo: base)
+      self = URL(filePath: path, directoryHint: directoryHint.directoryHint(), relativeTo: base)
     } else {
-      self = URL(fileURLWithPath: filePath, isDirectory: isDirectory)
+      self = URL(fileURLWithPath: path, isDirectory: directoryHint == .isDirectory)
     }
   }
 
+  @available(macOS, introduced: 10.10, obsoleted: 13.0, message: "")
   public func appending(component: some StringProtocol, directoryHint: URL._DirectoryHint = .inferFromPath) -> URL {
     if #available(macOS 13.0, *) {
       self.appending(component: component, directoryHint: directoryHint.directoryHint())
@@ -22,6 +23,7 @@ extension URL {
     }
   }
 
+  @available(macOS, introduced: 10.10, obsoleted: 13.0, message: "")
   public mutating func append(component: some StringProtocol, directoryHint: URL._DirectoryHint = .inferFromPath) {
     if #available(macOS 13.0, *) {
       self.append(component: component, directoryHint: directoryHint.directoryHint())
@@ -30,6 +32,7 @@ extension URL {
     }
   }
 
+  @available(macOS, introduced: 10.10, obsoleted: 13.0, message: "")
   public func appending(queryItems: [URLQueryItem]) -> URL {
     if #available(macOS 13.0, *) {
       Self.appending(queryItems:)(self)(queryItems)
@@ -37,12 +40,6 @@ extension URL {
       self._appending(queryItems: queryItems)
     }
   }
-}
-
-
-extension URL {
-
-  // MARK: Public
 
   public enum _DirectoryHint {
     case isDirectory
@@ -53,10 +50,10 @@ extension URL {
     @available(macOS 13, *)
     func directoryHint() -> DirectoryHint {
       switch self {
-        case .isDirectory: .isDirectory
-        case .notDirectory: .notDirectory
-        case .checkFileSystem: .checkFileSystem
-        case .inferFromPath: .inferFromPath
+      case .isDirectory: .isDirectory
+      case .notDirectory: .notDirectory
+      case .checkFileSystem: .checkFileSystem
+      case .inferFromPath: .inferFromPath
       }
     }
   }
@@ -71,3 +68,4 @@ extension URL {
     return url
   }
 }
+
--- Tests/RBKitTests/URL+_Tests.swift
+++ Tests/RBKitTests/URL+_Tests.swift
@@ -2,6 +2,12 @@ import XCTest
 @testable import RBKit
 
 final class BackportURL_Tests: XCTestCase {
+  func test_appending_queryItems() {
+    let actual = URL(fileURLWithPath: "/Users/roey/file.txt").appending(queryItems: [.init(name: "id", value: "foo")])
+    let expected = URL(string: "file:///Users/roey/file.txt?id=foo")
+    XCTAssertEqual(actual, expected)
+  }
+
   func test__appending_queryItems() {
     let actual = URL(fileURLWithPath: "/Users/roey/file.txt")._appending(queryItems: [.init(name: "id", value: "foo")])
     let expected = URL(string: "file:///Users/roey/file.txt?id=foo")

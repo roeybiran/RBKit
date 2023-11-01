dc9969324f60845a8b5500ca129081a3ecce5fa2
roeybiran <roeybiran@icloud.com>
2023-11-01 11:05:57 +0200

    add `host()`

--- Sources/RBKit/Foundation/URL+.swift
+++ Sources/RBKit/Foundation/URL+.swift
@@ -17,27 +17,35 @@ extension URL {
   @available(macOS, introduced: 10.10, obsoleted: 13.0, message: "")
   public func appending(component: some StringProtocol, directoryHint: URL._DirectoryHint = .inferFromPath) -> URL {
     if #available(macOS 13.0, *) {
-      self.appending(component: component, directoryHint: directoryHint.directoryHint())
+      appending(component: component, directoryHint: directoryHint.directoryHint())
     } else {
-      self.appendingPathComponent(String(component), isDirectory: directoryHint == .isDirectory)
+      appendingPathComponent(String(component), isDirectory: directoryHint == .isDirectory)
     }
   }
 
   @available(macOS, introduced: 10.10, obsoleted: 13.0, message: "")
   public mutating func append(component: some StringProtocol, directoryHint: URL._DirectoryHint = .inferFromPath) {
     if #available(macOS 13.0, *) {
-      self.append(component: component, directoryHint: directoryHint.directoryHint())
+      append(component: component, directoryHint: directoryHint.directoryHint())
     } else {
-      self.appendPathComponent(String(component), isDirectory: directoryHint == .isDirectory)
+      appendPathComponent(String(component), isDirectory: directoryHint == .isDirectory)
     }
   }
 
   @available(macOS, introduced: 10.10, obsoleted: 13.0, message: "")
   public func appending_(queryItems: [URLQueryItem]) -> URL {
     if #available(macOS 13.0, *) {
-      self.appending(queryItems: queryItems)
+      appending(queryItems: queryItems)
     } else {
-      self._appending(queryItems: queryItems)
+      _appending(queryItems: queryItems)
+    }
+  }
+
+  public func host_(percentEncoded: Bool = true) -> String? {
+    if #available(macOS 13.0, *) {
+      host(percentEncoded: percentEncoded)
+    } else {
+      URLComponents(url: self, resolvingAgainstBaseURL: false)?.host
     }
   }
 

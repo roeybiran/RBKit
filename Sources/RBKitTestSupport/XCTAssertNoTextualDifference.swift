54972ebaab0ec1447030b3144ada930e378de187
roeybiran <roeybiran@icloud.com>
2023-11-08 11:44:28 +0200

    wip

--- Sources/RBKitTestSupport/XCTAssertNoTextualDifference.swift
+++ Sources/RBKitTestSupport/XCTAssertNoTextualDifference.swift
@@ -1,38 +1,34 @@
 import XCTest
 
-public func XCTAssertNoTextualDifference<T>(
+public func XCTAssertNoDifference<T: Equatable>(
   _ expression1: T,
   _ expression2: T,
   showDiff: Bool = false,
   file: StaticString = #file,
   line: UInt = #line)
 {
+  let areEqual = expression1 == expression2
+  if areEqual { return }
+
+  XCTAssertTrue(areEqual, file: file, line: line)
+
   var output1 = ""
   var output2 = ""
 
   dump(expression1, to: &output1)
   dump(expression2, to: &output2)
 
-  XCTAssertEqual(output1, output2, file: file, line: line)
-
-  if showDiff, output1 != output2 {
-    showInFileMerge(output1, output2)
-  }
+  guard showDiff else { return }
+  showInFileMerge(output1, output2)
 }
 
 public func showInFileMerge(_ output1: String, _ output2: String) {
   let baseURL = FileManager.default.temporaryDirectory
-  let path1 = baseURL.appendingPathComponent("1").appendingPathExtension("txt").path
-  let path2 = baseURL.appendingPathComponent("2").appendingPathExtension("txt").path
+  let path1 = baseURL.appendingPathComponent("left").appendingPathExtension("txt").path
+  let path2 = baseURL.appendingPathComponent("right").appendingPathExtension("txt").path
   output1.write(toFile: path1)
   output2.write(toFile: path2)
   runShellScript(args: "/usr/bin/opendiff", path1, path2)
-}
-
-extension String {
-  fileprivate func write(toFile path: String) {
-    try? write(toFile: path, atomically: true, encoding: .utf8)
-  }
 }
 
 private func runShellScript(args: String...) {
@@ -43,3 +39,10 @@ private func runShellScript(args: String...) {
     try? task.run()
   }
 }
+
+extension String {
+  fileprivate func write(toFile path: String) {
+    try? write(toFile: path, atomically: true, encoding: .utf8)
+  }
+}
+

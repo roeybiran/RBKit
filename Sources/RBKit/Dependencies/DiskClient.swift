2292eb1080c06bcaddf94cc39ebe787fe3ec4b06
roeybiran <roeybiran@icloud.com>
2023-11-14 12:25:40 +0200

    wip

--- Sources/RBKit/Dependencies/DiskClient.swift
+++ Sources/RBKit/Dependencies/DiskClient.swift
@@ -3,32 +3,32 @@ import Foundation
 
 // MARK: - FileReadWriteClient
 
-public struct FileReadWriteClient {
+public struct DiskClient {
   public var read: (_ url: URL) throws -> Data
   public var write: (_ data: Data, _ url: URL, _ opts: Data.WritingOptions) throws -> Void
 }
 
 // MARK: DependencyKey
 
-extension FileReadWriteClient: DependencyKey {
-  public static let liveValue = FileReadWriteClient(
+extension DiskClient: DependencyKey {
+  public static let liveValue = DiskClient(
     read: { try Data(contentsOf: $0) },
     write: { data, url, opts in try data.write(to: url, options: opts) })
 
   #if DEBUG
-  public static let testValue = FileReadWriteClient(
+  public static let testValue = DiskClient(
     read: unimplemented("FileClient.read"),
     write: unimplemented("FileClient.write"))
 
-  public static let placeholder = FileReadWriteClient(
+  public static let placeholder = DiskClient(
     read: { _ in Data() },
     write: { _, _, _ in })
   #endif
 }
 
 extension DependencyValues {
-  public var fileClient: FileReadWriteClient {
-    get { self[FileReadWriteClient.self] }
-    set { self[FileReadWriteClient.self] = newValue }
+  public var fileClient: DiskClient {
+    get { self[DiskClient.self] }
+    set { self[DiskClient.self] = newValue }
   }
 }

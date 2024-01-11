94bc35b1d85810a43661e50e5bf9b38c208cdee8
roeybiran <roeybiran@icloud.com>
2024-01-11 12:55:06 +0200

    wip

--- Sources/RBKit/Dependencies/NotificationCenterClient.swift
+++ Sources/RBKit/Dependencies/NotificationCenterClient.swift
@@ -6,16 +6,22 @@ import Foundation
 
 @DependencyClient
 public struct NotificationCenterClient {
+  public var post: (_ notification: Notification) -> Void
   public var notifications: (_ name: Notification.Name, _ object: AnyObject?) -> NotificationCenter.Notifications = { NotificationCenter().notifications(named: $0, object: $1) }
 }
 
 // MARK: DependencyKey
 
 extension NotificationCenterClient: DependencyKey {
-  public static let liveValue = NotificationCenterClient(
-    notifications: { NotificationCenter.default.notifications(named: $0, object: $1) }
-  )
-  public static let testValue = NotificationCenterClient()
+  public static let liveValue: Self = {
+    let instance = NotificationCenter.default
+    return Self(
+      post: instance.post,
+      notifications: instance.notifications
+    )
+  }()
+
+  public static let testValue = Self()
 }
 
 extension DependencyValues {

1ea14740e5d8a338ae3d2ef2c15c852414452f83
roeybiran <roeybiran@icloud.com>
2024-01-22 13:57:16 +0200

    wip

--- Sources/RBKit/Dependencies/NSApplicationClient.swift
+++ Sources/RBKit/Dependencies/NSApplicationClient.swift
@@ -7,13 +7,38 @@ import DependenciesMacros
 @DependencyClient
 public struct NSApplicationClient {
   public var terminate: (_ sender: Any?) -> Void
+  @DependencyEndpoint(method: "yieldActivation")
+  public var yieldActivationTo: (_ to: NSRunningApplication) -> Void
+  @DependencyEndpoint(method: "yieldActivation")
+  public var yieldActivationToApplicationWithBundleIdentifier: (_ toApplicationWithBundleIdentifier: String) -> Void
 }
 
 // MARK: DependencyKey
 
 extension NSApplicationClient: DependencyKey {
-  public static let liveValue = NSApplicationClient(terminate: { sender in NSApplication.shared.terminate(sender) })
-  public static let testValue = NSApplicationClient()
+  public static let liveValue: Self = {
+    let instance = NSApplication.shared
+    return Self(
+      terminate: { sender in
+        instance.terminate(sender)
+      },
+      yieldActivationTo: { app in
+        if #available(macOS 14.0, *) {
+          instance.yieldActivation(to: app)
+        } else {
+          // Fallback on earlier versions
+        }
+      },
+      yieldActivationToApplicationWithBundleIdentifier: { bundleID in
+        if #available(macOS 14.0, *) {
+          instance.yieldActivation(toApplicationWithBundleIdentifier: bundleID)
+        } else {
+          // Fallback on earlier versions
+        }
+      }
+    )
+  }()
+  public static let testValue = Self()
 }
 
 extension DependencyValues {
---
+++ Sources/RBKit/Dependencies/NSRunningApplicationClient.swift
@@ -0,0 +1,42 @@
+import AppKit.NSApplication
+import Dependencies
+import DependenciesMacros
+
+// MARK: - NSApplicationClient
+
+@DependencyClient
+public struct NSRunningApplicationClient {
+  public var runningApplications: (_ withBundleIdentifier: String) -> [NSRunningApplication] = { _ in [] }
+  public var current: () -> NSRunningApplication = { .init() }
+  @DependencyEndpoint(method: "activate")
+  public var activate: (_ app: NSRunningApplication, _ options: NSApplication.ActivationOptions) -> Bool = { _, _ in false }
+  @DependencyEndpoint(method: "activate")
+  public var activateFromApplication: (_ app: NSRunningApplication, _ fromApp: NSRunningApplication, _ options: NSApplication.ActivationOptions) -> Bool = { _, _, _ in false }
+}
+
+// MARK: DependencyKey
+
+extension NSRunningApplicationClient: DependencyKey {
+  public static let liveValue = NSRunningApplicationClient(
+    runningApplications: NSRunningApplication.runningApplications,
+    current: { NSRunningApplication.current },
+    activate: { app, options in
+      app.activate(options: options)
+    },
+    activateFromApplication: { app, fromApp, options in
+      if #available(macOS 14.0, *) {
+        app.activate(from: fromApp, options: options)
+      } else {
+        app.activate(options: options)
+      }
+    }
+  )
+  public static let testValue = NSRunningApplicationClient()
+}
+
+extension DependencyValues {
+  public var nsRunningApplicationClient: NSRunningApplicationClient {
+    get { self[NSRunningApplicationClient.self] }
+    set { self[NSRunningApplicationClient.self] = newValue }
+  }
+}

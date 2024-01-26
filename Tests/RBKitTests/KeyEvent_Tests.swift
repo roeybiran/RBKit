792e77f808272fce3d46322f67d88961742b3dba
roeybiran <roeybiran@icloud.com>
2024-01-27 01:11:52 +0200

    fix tests

--- Tests/RBKitTests/KeyEvent_Tests.swift
+++ Tests/RBKitTests/KeyEvent_Tests.swift
@@ -3,26 +3,26 @@ import XCTest
 
 final class KeyEvent_Tests: XCTestCase {
   func test_keyEventInit() {
-    let actual = KeyEvent(keyCode: 0, characters: "a", charactersIgnoringModifiers: "a")
-    let expected = KeyEvent(
-      keyCode: 0,
+    let actual = NSEventValue(characters: "a", charactersIgnoringModifiers: "a", keyCode: 0)
+    let expected = NSEventValue(
+      _type: .keyDown,
+      modifierFlags: [],
       characters: "a",
       charactersIgnoringModifiers: "a",
-      type: .keyDown,
-      modifiers: [],
-      repeating: false)
+      isARepeat: false
+    )
     XCTAssertEqual(actual, expected)
   }
 
   func test_keyEventDownArrow() {
-    let actual = KeyEvent.downArrow()
+    let actual = NSEventValue.downArrow()
     XCTAssertEqual(actual.keyCode, 125)
     XCTAssertEqual(actual.characters?.count, 1)
     XCTAssertEqual(actual.charactersIgnoringModifiers?.count, 1)
   }
 
   func test_keyEventUpArrow() {
-    let actual = KeyEvent.upArrow()
+    let actual = NSEventValue.upArrow()
     XCTAssertEqual(actual.keyCode, 126)
     XCTAssertEqual(actual.characters?.count, 1)
     XCTAssertEqual(actual.charactersIgnoringModifiers?.count, 1)

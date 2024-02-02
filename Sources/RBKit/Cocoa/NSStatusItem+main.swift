0f807d69460698e3fbc0dceca06fcc22431b210a
roeybiran <roeybiran@icloud.com>
2024-02-02 02:43:43 +0200

    wip

---
+++ Sources/RBKit/Cocoa/NSStatusItem+main.swift
@@ -0,0 +1,23 @@
+import AppKit
+
+extension NSStatusItem {
+  public static func main(
+    menu: NSMenu = NSMenu(),
+    length: CGFloat = NSStatusItem.variableLength,
+    statusBar: NSStatusBar = .system,
+    imageName: String = "StatusItem")
+    -> NSStatusItem
+  {
+    let statusItem = statusBar.statusItem(withLength: length)
+    statusItem.button?.image = NSImage(named: imageName)
+    statusItem.button?.image?.isTemplate = true
+    statusItem.button?.target = statusItem
+    statusItem.menu = menu
+    #if DEBUG
+    statusItem.button?.title = "DEV"
+    #elseif PROFILE
+    statusItem.button?.title = "PRF"
+    #endif
+    return statusItem
+  }
+}

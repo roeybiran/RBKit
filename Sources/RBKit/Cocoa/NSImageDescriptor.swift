87025b4f43263315c5fc9955b9bd8a3cbb13e5b2
roeybiran <roeybiran@icloud.com>
2024-02-05 16:08:44 +0200

    wip

---
+++ Sources/RBKit/Cocoa/NSImageDescriptor.swift
@@ -0,0 +1,24 @@
+import AppKit
+import UniformTypeIdentifiers
+
+public enum NSImageDescriptor: Equatable {
+  case name(String)
+  case systemSymbol(name: String, accessibilityDescription: String?)
+  case contentType(UTType)
+  case path(String)
+}
+
+extension NSImage {
+  public static func from(descriptor: NSImageDescriptor, workspace: NSWorkspace = .shared) -> NSImage? {
+    switch descriptor {
+    case .name(let name):
+      NSImage(named: name)
+    case .systemSymbol(let systemSymbolName, let accessibilityDescription):
+      NSImage(systemSymbolName: systemSymbolName, accessibilityDescription: accessibilityDescription)
+    case .contentType(let contentType):
+      workspace.icon(for: contentType)
+    case .path(let path):
+      workspace.icon(forFile: path)
+    }
+  }
+}

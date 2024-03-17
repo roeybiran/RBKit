e0c6e267cf70ea169978a8a0bfbb15fd5d047aa3
roeybiran <roeybiran@icloud.com>
2024-03-17 11:41:00 +0200

    add public

---
+++ Sources/RBKit/Cocoa/NSTableView.AnimationOptions+CustomDebugStringConvertible.swift
@@ -0,0 +1,19 @@
+import Cocoa
+import RBKit
+
+extension NSTableView.AnimationOptions: CustomDebugStringConvertible {
+  public var debugDescription: String {
+    [
+      (Self.effectFade, "effectFade"),
+      (.effectGap, "effectGap"),
+      (.slideUp, "slideUp"),
+      (.slideDown, "slideDown"),
+      (.slideLeft, "slideLeft"),
+      (.slideRight, "slideRight"),
+    ]
+    .filter { contains($0.0) }
+    .map(\.1)
+    .joined(separator: ", ")
+    .replaceEmpty(with: "<NONE>")
+  }
+}
--- Sources/RBKit/Extensions/stdlib/Dictionary+.swift
+++ Sources/RBKit/Extensions/stdlib/Dictionary+.swift
@@ -1,11 +1,11 @@
 extension Dictionary {
-  subscript(key: Key?) -> Value? {
+  public subscript(key: Key?) -> Value? {
     key.map { self[$0] } ?? nil
   }
 }
 
 extension Dictionary where Value: RangeReplaceableCollection {
-  subscript(key: Key?) -> Value {
+  public subscript(key: Key?) -> Value {
     key.map { self[$0] ?? Value() } ?? Value()
   }
 }

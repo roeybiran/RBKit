ce7285c427450449f2c40fec4748ae4fd694f698
roeybiran <roeybiran@icloud.com>
2024-06-04 13:03:39 +0300

    w

--- Sources/RBKit/Dependencies/NSEventClient/NSEventClient.swift
+++ Sources/RBKit/Dependencies/NSEventClient/NSEventClient.swift
@@ -5,7 +5,6 @@ import DependenciesMacros
 // MARK: - EventMonitor
 
 @DependencyClient
-@MainActor
 public struct NSEventClient {
   public typealias LocalEventsStream = AsyncStream<(NSEvent, (_ event: NSEvent?) -> Void)>
   public var mouseLocation: () -> NSPoint = { .zero }

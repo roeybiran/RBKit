e11b093e394e2f1312f8184b823403334fb9c415
roeybiran <roeybiran@icloud.com>
2024-06-25 10:23:24 +0300

    w

--- Sources/RBKit/Dependencies/NotificationCenterClient.swift
+++ Sources/RBKit/Dependencies/NotificationCenterClient.swift
@@ -7,10 +7,7 @@ import Foundation
 @DependencyClient
 public struct NotificationCenterClient {
   public var post: (_ notification: Notification) -> Void
-  public var notifications: (_ named: Notification.Name, _ object: AnyObject?) -> NotificationCenter
-    .Notifications = { NotificationCenter().notifications(
-      named: $0,
-      object: $1) }
+  public var notifications: (_ named: Notification.Name, _ object: AnyObject?) -> AsyncStream<Notification> = { _, _ in .finished }
 }
 
 // MARK: DependencyKey
@@ -20,7 +17,10 @@ extension NotificationCenterClient: DependencyKey {
     let instance = NotificationCenter.default
     return Self(
       post: instance.post,
-      notifications: instance.notifications)
+      notifications: {
+        instance.notifications(named: $0, object: $1).eraseToStream()
+      }
+    )
   }()
 
   public static let testValue = Self()

87914daf228f728bcee20951d0acc7be99f9f9fe
roeybiran <roeybiran@icloud.com>
2024-07-06 14:36:40 +0300

    w

---
+++ Sources/RBKit/Dependencies/ScreenCaptureClient.swift
@@ -0,0 +1,32 @@
+import Dependencies
+import DependenciesMacros
+import ScreenCaptureKit
+
+@available(macOS 12.3, *)
+@DependencyClient
+public struct ScreenCaptureClient {
+  public var current: () async throws -> SCShareableContent
+  public var requestAccess: () async -> Void
+}
+
+@available(macOS 12.3, *)
+extension ScreenCaptureClient: DependencyKey {
+  public static let liveValue: Self = {
+    .init(
+      current: { try await SCShareableContent.current },
+      requestAccess: {
+        _ = try? await SCShareableContent.current
+      }
+    )
+  }()
+
+  public static let testValue = Self()
+}
+
+@available(macOS 12.3, *)
+extension DependencyValues {
+  public var screenCaptureClient : ScreenCaptureClient {
+    get { self[ScreenCaptureClient.self] }
+    set { self[ScreenCaptureClient.self] = newValue }
+  }
+}

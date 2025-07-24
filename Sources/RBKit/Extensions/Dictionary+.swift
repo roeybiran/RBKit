385fa288707d6c3f22cae7dda25d7377887c01ac
Roey Biran <roeybiran@icloud.com>
2025-07-24 12:32:00 +0300

    wip

--- Sources/RBKit/Extensions/Dictionary+.swift
+++
@@ -1,9 +0,0 @@
-extension Dictionary {
-  public subscript(key: Key?) -> Value? {
-    key.map { self[$0] } ?? nil
-  }
-
-  public subscript(key: Key?, default defaultValue: @autoclosure () -> Value) -> Value {
-    key.map { self[$0, default: defaultValue()] } ?? defaultValue()
-  }
-}

e7b980f8346c6457a809092045c3041e57a3e2c0
Roey Biran <roeybiran@icloud.com>
2025-11-25 12:29:14 +0200

    Add CGFloat.standard to Constants and add comment to Bundle.appName

--- Sources/RBKit/Constants/Constants.swift
+++ Sources/RBKit/Constants/Constants.swift
@@ -6,4 +6,6 @@ extension CGFloat {
   public static let INTER_CONTROL_SPACING = Self(6)
   public static let INTER_GROUP_SPACING = Self(12)
   public static let DIVIDER_SPACING = Self(12) * Self(2)
+  /// The standard (or default) length used for constraints in Interface Builder (as of Xcode 15.2).
+  public static let standard = 20.0
 }

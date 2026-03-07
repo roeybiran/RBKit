47c26693445da2341180be703e7a512a67735124
Roey Biran <roeybiran@icloud.com>
2026-03-07 18:24:14 +0200

    Adjust NSMenu extension API and formatting

--- Sources/RBKit/Extensions/NSMenu+.swift
+++ Sources/RBKit/Extensions/NSMenu+.swift
@@ -4,50 +4,26 @@ extension NSMenu {
 
   // MARK: Lifecycle
 
-  public convenience init(
-    _ title: String,
-    @MenuBuilder builder: () -> [NSMenuItem] = {
-      []
-    }
-  ) {
-    self.init(
-      title: title
-    )
+  public convenience init( _ title: String, @MenuBuilder builder: () -> [NSMenuItem] = { [] }) {
+    self.init(title: title)
     items = builder()
   }
 
   // MARK: Public
 
   @MainActor
-  public static func fromNib(
-    named nibName: String,
-    bundle: Bundle = .main
-  ) -> NSMenu {
-    let nib = NSNib(
-      nibNamed: nibName,
-      bundle: bundle
-    )
+  public static func fromNib(named nibName: String, bundle: Bundle = .main) -> NSMenu {
+    let nib = NSNib(nibNamed: nibName, bundle: bundle)
     var objects: NSArray?
-    nib?
-      .instantiate(
-        withOwner: NSApplication.shared.delegate,
-        topLevelObjects: &objects
-      )
-    return (
-      (
-        objects as? [Any]
-      )?.first(
-        where: {
-          $0 is NSMenu
-        }
-      ) as? NSMenu
-    ) ?? .init()
+    nib?.instantiate( withOwner: NSApplication.shared.delegate, topLevelObjects: &objects)
+    return (( objects as? [Any])?.first(where: { $0 is NSMenu }) as? NSMenu) ?? .init()
   }
 
   // MARK: Internal
 
   /// A programmatically created application menu bar (`NSApp.mainMenu`). The menu’s structure and contents are an exact clone of those in a Storyboard–based, non–modified menu bar (as of **Xcode 14.2**). This is not meant to be used directly in your project (hence the lack of the `public` modifier), but rather copied and modified to suit your needs.
-  @MainActor static let standardMenuBar = NSMenu(String.appName) {
+  @MainActor
+  public static let standardMenuBar = NSMenu(String.appName) {
     NSMenuItem.applicationMenu
     NSMenuItem.fileMenu
     NSMenuItem.editMenu
@@ -55,5 +31,10 @@ extension NSMenu {
     NSMenuItem.viewMenu
     NSMenuItem.windowMenu()
     NSMenuItem.helpMenu()
+  }
+
+  public func withChildren(@MenuBuilder _ builder: () -> [NSMenuItem]) -> Self {
+    items = builder()
+    return self
   }
 }

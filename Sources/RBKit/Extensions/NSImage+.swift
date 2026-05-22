017f5d442cee45df85c0c35778700ac16fca2970
Roey Biran <roeybiran@icloud.com>
2026-05-22 14:26:26 +0300

    Document NSImage tint source

--- Sources/RBKit/Extensions/NSImage+.swift
+++ Sources/RBKit/Extensions/NSImage+.swift
@@ -1,6 +1,7 @@
 import AppKit
 
 extension NSImage {
+  /// Source: https://stackoverflow.com/questions/1413135/tinting-a-grayscale-nsimage-or-ciimage
   public func draw(rect: CGRect, tint color: NSColor) {
     let tintedImage = NSImage(size: size, flipped: false) { bounds in
       self.draw(in: bounds, from: bounds, operation: .sourceOver, fraction: color.alphaComponent)

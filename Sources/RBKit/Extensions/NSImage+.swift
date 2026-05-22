85ac573bfc18e618596c526ac9a5d7d34e4eb340
Roey Biran <roeybiran@icloud.com>
2026-05-22 14:22:47 +0300

    Update NSImage tint drawing

--- Sources/RBKit/Extensions/NSImage+.swift
+++ Sources/RBKit/Extensions/NSImage+.swift
@@ -3,7 +3,7 @@ import AppKit
 extension NSImage {
   public func draw(rect: CGRect, tint color: NSColor) {
     let tintedImage = NSImage(size: size, flipped: false) { bounds in
-      self.draw(in: bounds)
+      self.draw(in: bounds, from: bounds, operation: .sourceOver, fraction: color.alphaComponent)
       color.setFill()
       bounds.fill(using: .sourceAtop)
       return true
